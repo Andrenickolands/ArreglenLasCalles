191532d80a3f61d6eb5e745480d25164b4f3b4b3
Andrenickolands <nicosepulvedaamaya@gmail.com>
2025-02-24 17:02:40 -0500

    Nicolas Rutas

--- src/app/Views/Login/login.component.ts
+++ src/app/Views/Login/login.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+// import { userLogin } from '../../core/interfaces/login';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+
+export class LoginComponent implements OnInit {
+  ocultarConfirmacionContrasenia: boolean = true;
+  ocultarContrasenia: boolean = true;
+
+  loginForm: FormGroup;
+
+  constructor(formBuilder: FormBuilder, private router: Router) {
+    this.loginForm = formBuilder.group({
+      correoUsuario: ["", Validators.required, Validators.email],
+      password: ["", Validators.required],
+    })
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    //   this.loginForm.markAllAsTouched();
+
+    //   if (this.loginForm.valid) {
+    //     console.log('logeado');
+
+    //     const user: userLogin = {
+    //       idusuario: 0,
+    //       correoUsuario: this.loginForm.get('correoUsuario')?.value,
+    //       password: this.loginForm.get('password')?.value
+    //     }
+
+    //     console.log('Username');
+    //     console.log(user);
+
+    //     console.log('form');
+    //     console.log(this.loginForm);
+
+    //     // this.router.navigateByUrl('/home');
+    //     this.router.navigateByUrl('/dashboard');
+    //     this.loginForm.reset();
+    //   } else {
+
+    //   }
+  }
 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Views/Login/login.component';
+import { DashboardComponent } from './Views/DashBoard/dashboard.component';
+import { HomeComponent } from './Views/Home/home.component';
+import { SignupComponent } from './Views/Login/SignUp/signup.component';
+import { AceptTermsComponent } from './Views/Login/SignUp/AceptPolitics/acept-terms.component';
+import { LostpassworeComponent } from './Views/Login/LostPasswore/lostpasswore.component';
+import { SearchComponent } from './Views/Search/search.component';
+import { HelpComponent } from './Views/help/help.component';
+import { ProfileComponent } from './Views/profile/profile.component';
+import { ReportsComponent } from './Views/reports/reports.component';
+import { ReportComponent } from './Views/reports/report/report.component';
+import { BacheComponent } from './Views/reports/bache/bache.component';
+import { CreatedReportComponent } from './Views/created-report/created-report.component';
+import { ReportValidateComponent } from './Views/created-report/report-validate/report-validate.component';
+import { AddReportComponent } from './Views/add-report/add-report.component';
+import { PinSelectComponent } from './Views/add-report/pin-select/pin-select.component';
+import { CreateReportComponent } from './Views/add-report/create-report/create-report.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"dashboard", component: DashboardComponent},
+    {path:"login", component: LoginComponent},
+    {path:"Signup", component: SignupComponent},
+    {path:"Acept-terms", component: AceptTermsComponent},
+    {path:"Lost-passwore", component: LostpassworeComponent},
+    {path:"Home", component: HomeComponent},
+    {path:"Help", component: HelpComponent},
+    {path:"Profile", component: ProfileComponent},
+    {path:"Search", component: SearchComponent},
+    {path:"Reports", component: ReportsComponent},
+    {path:"Report", component: ReportComponent},
+    {path:"Bache", component: BacheComponent},
+    {path:"Created-report", component: CreatedReportComponent},
+    {path:"Validated-report", component: ReportValidateComponent},
+    {path:"Add-report", component: AddReportComponent},
+    {path:"Pin-select", component: PinSelectComponent},
+    {path:"Create-report", component: CreateReportComponent},
+    {path: "", component: HomeComponent},
+    {path:"**", redirectTo:"", pathMatch:"full"},
+];
