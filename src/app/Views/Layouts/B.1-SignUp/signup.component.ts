490a3bd5ca94addbad72e4f1be60aa49b9b05537
Andrenickolands <nicosepulvedaamaya@gmail.com>
2025-03-04 00:31:41 -0500

    Nicolas SignUp

--- src/app/Views/Layouts/B.1-SignUp/signup.component.ts
+++ src/app/Views/Layouts/B.1-SignUp/signup.component.ts
@@ -4,15 +4,16 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CustomValidators } from '../../../../Communs/Custom-validators';
-import { User } from '../../../Models/User';
 import { InptPrincipalComponent } from '../../Components/Inputs/inpt-principal/inpt-principal.component';
 import { InptDateComponent } from '../../Components/Inputs/inpt-date/inpt-date.component';
+import { InptEmailComponent } from '../../Components/Inputs/inpt-email/inpt-email.component';
+import { InptPasswordComponent } from '../../Components/Inputs/inpt-password/inpt-password.component';
 
 
 
 @Component({
   selector: 'app-signup',
-  imports: [CommonModule, ReactiveFormsModule, InptPrincipalComponent, InptDateComponent],
+  imports: [CommonModule, ReactiveFormsModule, /*InptPrincipalComponent, InptDateComponent, InptEmailComponent, InptPasswordComponent */],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
@@ -27,7 +28,6 @@ export class SignupComponent implements OnInit {
     birthdate: new FormControl(''),
     password: new FormControl(''),
     passwordConfirm: new FormControl(''),
-    aceptTyCUser: new FormControl(''),
   });
 
   constructor(formBuilder: FormBuilder, private router: Router) {
@@ -39,52 +39,49 @@ export class SignupComponent implements OnInit {
       birthdate: ["", Validators.required],
       password: ["", Validators.required, Validators.minLength(8), CustomValidators.passw],
       passwordConfirm: ["", Validators.required, CustomValidators.mustBeEqual('password', 'passwordConfirm')],
-      aceptTyCUser: ["", Validators.required],
     })
   }
 
   ngOnInit(): void {
     throw new Error('Method not implemented.');
   }
+
+  submitForm() {
+    this.signUpUserForm.markAllAsTouched();
+    this.signUp();
+  }
+
+  signUp() {
+
+    if (this.signUpUserForm.valid) {
+
+      this.showErrors = false;
+
+      console.log('usuario creado');
+
+      const user = {
+        iduser: 0,
+        name: this.signUpUserForm.get('name')?.value,
+        email: this.signUpUserForm.get('email')?.value,
+        country: this.signUpUserForm.get('country')?.value,
+        birthdate: this.signUpUserForm.get('birthdate')?.value,
+        password: this.signUpUserForm.get('password')?.value,
+        passwordConfirm: this.signUpUserForm.get('passwordConfirm')?.value,
+      }
+
+      console.log('persona');
+      console.log(user);
+
+      console.log('form');
+      console.log(this.signUpUserForm);
+
+      this.router.navigate(['/Acept-terms', user.iduser, user.name, user.email, user.country, user.birthdate, user.password, user.passwordConfirm, user.email]); 
+
+    } else {
+      this.showErrors = true;
+      return;
+    }
+
+  }
+
 }
-
-//   submitForm() {
-//     this.signUpUserForm.markAllAsTouched();
-//     this.signUp();
-//   }
-
-//   signUp() {
-
-//     if (this.signUpUserForm.valid) {
-
-//       this.showErrors = false;
-
-//       console.log('usuario creado');
-
-//       const user: User = {
-//         iduser: 0,
-//         name: this.signUpUserForm.get('name')?.value,
-//         email: this.signUpUserForm.get('email')?.value,
-//         country: this.signUpUserForm.get('country')?.value,
-//         birthdate: this.signUpUserForm.get('birthdate')?.value,
-//         password: this.signUpUserForm.get('password')?.value,
-//         passwordConfirm: this.signUpUserForm.get('passwordConfirm')?.value,
-//         aceptTyCUser: this.signUpUserForm.get('aceptTyCUser')?.value
-//       }
-
-//       console.log('persona');
-//       console.log(user);
-
-//       console.log('form');
-//       console.log(this.signUpUserForm);
-
-//       this.router.navigateByUrl('/Acept-terms');
-
-//     } else {
-//       this.showErrors = true;
-//       return;
-//     }
-
-//   }
-
-// }
