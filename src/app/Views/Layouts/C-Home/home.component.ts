98635b9eedb5ffe50345c674c4e56a7c971b076a
Andrenickolands <nicosepulvedaamaya@gmail.com>
2025-03-09 17:19:24 -0500

    Home

--- src/app/Views/Layouts/C-Home/home.component.ts
+++ src/app/Views/Layouts/C-Home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../Components/Footer/footer.component";
+import { HeaderSesionComponent } from "../../Components/Header/Header-sesion/header-sesion.component";
+import { PaisComponent } from "../../Components/Reports/Pais/pais.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FooterComponent, HeaderSesionComponent, PaisComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
