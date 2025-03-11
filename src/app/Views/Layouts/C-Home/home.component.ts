383b312dd0dccda7581b2125add50b6ca084873f
Andrenickolands <nicosepulvedaamaya@gmail.com>
2025-03-11 10:38:09 -0500

    profile

--- src/app/Views/Layouts/C-Home/home.component.ts
+++ src/app/Views/Layouts/C-Home/home.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { FooterComponent } from "../../Components/Footer/footer.component";
 import { HeaderSesionComponent } from "../../Components/Header/Header-sesion/header-sesion.component";
 import { PaisComponent } from "../../Components/Reports/Pais/pais.component";
-import { HeaderDashboardComponent } from "../../Components/Header/Header-dashboard/header-dashboard.component";
 
 @Component({
   selector: 'app-home',
-  imports: [FooterComponent, HeaderSesionComponent, PaisComponent, HeaderDashboardComponent],
+  imports: [FooterComponent, HeaderSesionComponent, PaisComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -37,6 +37,6 @@ export const routes: Routes = [
     {path:"Add-report", component: AddReportComponent},
     {path:"Pin-select", component: PinSelectComponent},
     {path:"Create-report", component: CreateReportComponent},
-    {path: "", component: HomeComponent},
+    {path: "", component: ProfileComponent},
     {path:"**", redirectTo:"", pathMatch:"full"},
 ];
