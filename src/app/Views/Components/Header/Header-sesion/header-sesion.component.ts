927121c85eed2f8b93ccb071fa9805d7ef4b8631
Andrenickolands <nicosepulvedaamaya@gmail.com>
2025-04-05 18:27:39 -0500

    Nicolas togle menu

--- src/app/Views/Components/Header/Header-sesion/header-sesion.component.ts
+++ src/app/Views/Components/Header/Header-sesion/header-sesion.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header-sesion',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header-sesion.component.html',
   styleUrl: './header-sesion.component.css'
 })
-export class HeaderSesionComponent {
+export class HeaderSesionComponent implements OnInit {
+  isMenuOpen = false;
+
+  ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    console.log('Estado anterior:', this.isMenuOpen);
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log('Nuevo estado:', this.isMenuOpen);
+  }
+
+  Start(){
+
+  }
+
+  WatchReports(){
+    
+  }
+
+  Profile(){
+
+  }
+
+  Help(){
+    
+  }
+
+  Donate(){
+
+  }
+
+  LogOut(){
+    
+  }
 
 }
