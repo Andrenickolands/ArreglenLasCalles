d76437782faf4f2b1ef14a1018132dde34cd2c9a
Andrenickolands <nicosepulvedaamaya@gmail.com>
2025-04-05 13:23:26 -0500

    Nicolas createdReport

--- src/app/Views/Layouts/C.1-created-report/created-report.component.ts
+++ src/app/Views/Layouts/C.1-created-report/created-report.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { CustomValidators } from '../../../../Communs/Custom-validators';
+import { CommonModule } from '@angular/common';
+import { HeaderSesionComponent } from '../../Components/Header/Header-sesion/header-sesion.component';
 
 @Component({
   selector: 'app-created-report',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, HeaderSesionComponent],
   templateUrl: './created-report.component.html',
   styleUrl: './created-report.component.css'
 })
-export class CreatedReportComponent {
+export class CreatedReportComponent implements OnInit {
+  form: FormGroup;
+  showErrors: boolean = false;
+
+  constructor(formBuilder: FormBuilder) {
+
+    this.form = formBuilder.group({
+      direction: ["", Validators.required],
+      neighborhood: ["", Validators.required],
+      sector: ["", Validators.required],
+      city: ["", Validators.required],
+      country: ["", Validators.required],
+      dateReport: ["", Validators.required],
+    })
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  submitForm() {
+    this.form.markAllAsTouched();
+  }
+
+  isFieldInvalid(fieldName: string): boolean {
+    const field = this.form.get(fieldName);
+    return field ? (field.invalid && (field.dirty || field.touched)) : false;
+  }
+
+  Vote() {
+   
+  }
 
 }
