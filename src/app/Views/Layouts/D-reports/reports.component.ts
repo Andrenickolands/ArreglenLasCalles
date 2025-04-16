1972f1c0ed7271f7e40bce69d30214a5ddb87218
Andrenickolands <nicosepulvedaamaya@gmail.com>
2025-04-16 16:26:38 -0500

    Nicolas reports

--- src/app/Views/Layouts/D-reports/reports.component.ts
+++ src/app/Views/Layouts/D-reports/reports.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderSesionComponent } from '../../Components/Header/Header-sesion/header-sesion.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reports',
-  imports: [],
+  imports: [HeaderSesionComponent, CommonModule],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -37,7 +37,7 @@ export const routes: Routes = [
     {path:"Add-report", component: AddReportComponent},
     {path:"Pin-select", component: PinSelectComponent},
     {path:"Create-report", component: CreateReportComponent},
-    {path: "", component: CreateReportComponent},
+    {path: "", component: ReportsComponent},
     {path:"**", redirectTo:"", pathMatch:"full"},
 ];
 
