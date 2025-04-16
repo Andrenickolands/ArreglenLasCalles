2725bb4120c82419c60b05db7d783d02a35c7129
Andrenickolands <nicosepulvedaamaya@gmail.com>
2025-04-16 11:43:02 -0500

    Nicolas create report

--- src/app/Views/Layouts/C.2.2-create-report/create-report.component.ts
+++ src/app/Views/Layouts/C.2.2-create-report/create-report.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { FooterComponent } from "../../Components/Footer/footer.component";
-import { HeaderDashboardComponent } from "../../Components/Header/Header-dashboard/header-dashboard.component";
+import { HeaderSesionComponent } from '../../Components/Header/Header-sesion/header-sesion.component';
+import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-create-report',
-  imports: [FooterComponent, HeaderDashboardComponent],
+  standalone: true,
+  imports: [FooterComponent, HeaderSesionComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './create-report.component.html',
   styleUrl: './create-report.component.css'
 })
-export class CreateReportComponent {
+export class CreateReportComponent implements OnInit {
+  form: FormGroup;
+  showErrors: boolean = false;
+  mapUrl: string = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7953.326118062753!2d-74.11876906570242!3d4.6540410747946135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9b8e3f0b6cd7%3A0x2751387a49099904!2sCiudad%20Salitre%2C%20Teusaquillo%2C%20Bogot%C3%A1%2C%20Bogota!5e0!3m2!1sen!2sco!4v1698713434561!5m2!1sen!2sco';
+  safeMapUrl!: SafeResourceUrl;
+
+  constructor(formBuilder: FormBuilder, private sanitizer: DomSanitizer) {
+
+    this.form = formBuilder.group({
+      direction: ["", Validators.required],
+      neighborhood: ["", Validators.required],
+      sector: ["", Validators.required],
+      city: ["", Validators.required],
+      country: ["", Validators.required],
+    })
+  }
+
+  ngOnInit() {
+    this.safeMapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.mapUrl);
+  }
+
+  submitForm() {
+    this.form.markAllAsTouched();
+  }
+
+  isFieldInvalid(fieldName: string): boolean {
+    const field = this.form.get(fieldName);
+    return field ? (field.invalid && (field.dirty || field.touched)) : false;
+  }
+
+  Report() {
+   
+  }
 
 }
